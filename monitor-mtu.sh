754ade482c0ec397460665f5bcc081018798034b
Bernd Schorgers <me@bjw-s.dev>
2026-03-20 16:55:54 +0100

    fix: Prevent infinite loop

--- monitor-mtu.sh
+++ monitor-mtu.sh
@@ -11,23 +11,26 @@ fi
 attempt=1
 MTUPATH="/sys/class/net/${PPP_INTERFACE}/mtu"
 
-while [ ! -f "${MTUPATH}" ];
+while [[ ! -f "${MTUPATH}" ]];
 do
 	echo "${PPP_INTERFACE} device not ready - attempt #$attempt"
 	(( attempt++ ))
 	sleep 15
 done
 
-INTERFACE_MTU=$(cat "$MTUPATH")
+INTERFACE_MTU=$(cat "${MTUPATH}")
 echo "MTU for ${PPP_INTERFACE} on startup is ${INTERFACE_MTU}"
 
-if [ "${INTERFACE_MTU}" -ne ${MTU} ]; then
+if [[ "${INTERFACE_MTU}" -ne ${MTU} ]]; then
 	/data/fix-mtu/fix-mtu.sh
 else
 	ip monitor link | while read -r line; do
-	  if [[ "$line" == *"${PPP_INTERFACE}"* && "$line" == *"mtu"* ]]; then
-  		echo "MTU change detected on ${PPP_INTERFACE}: $line"
-  		/data/fix-mtu/fix-mtu.sh
+	  if [[ "${line}" == *"${PPP_INTERFACE}"* ]]; then
+	    INTERFACE_MTU=$(cat "${MTUPATH}")
+	    if [[ "${INTERFACE_MTU}" -ne "${MTU}" ]]; then
+  		  echo "MTU change detected on ${PPP_INTERFACE}: ${line}"
+  		  /data/fix-mtu/fix-mtu.sh
+	    fi
 	  fi
 	done
 fi
